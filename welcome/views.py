5850e788e523638075be841dc827a74e3950b526
Yuta OGIHARA <ogihara@bbt757.com>
2012-03-09 15:38:56 +0900

    取りあえずPDFが出せる

--- welcome/views.py
+++ welcome/views.py
@@ -13,13 +13,15 @@ from kay.utils import render_to_response
 def test_conversion():
     from google.appengine.api import conversion
 
-    asset = conversion.Asset('text/html', '<b>some data</b>', 'index.html')
-    conversion_obj = conversion.Conversion(asset, 'image/png')
+    asset = conversion.Asset('text/html', '<b>some data</b><br><b>aaaaaa</b>', 'index.html')
+    conversion_obj = conversion.Conversion(asset, 'application/pdf')
 
-    rpc = conversion.create_rpc()
-    conversion.make_convert_call(rpc,  conversion_obj)
+    # async
+    #rpc = conversion.create_rpc()
+    #conversion.make_convert_call(rpc,  conversion_obj)
+    #result = rpc.get_result()
 
-    result = rpc.get_result()
+    result = conversion.convert(conversion_obj)
     if result.assets:
         return result.assets[0].data
     else:
@@ -29,4 +31,4 @@ def index(request):
     return render_to_response('welcome/index.html', {'message': 'Hello'})
 
 def makepdf(request):
-    return Response(test_conversion(), status=200)
+    return Response(test_conversion(), status=200, content_type='application/pdf')
